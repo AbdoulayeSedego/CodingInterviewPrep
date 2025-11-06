7de77c349d917b2f6c5186f5a96af9e79ad64390
abdoulayesedego <abdoulaye.sedego95@gmail.com>
2025-11-06 01:16:36 -0500

    solved problem 3

--- p2.py
+++ p2.py
@@ -1,12 +1,14 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # Your code here
-        s = ''.join(sorted(s))
-        t = ''.join(sorted(t))
+        # # split and sort the string
+        # s = ''.join(sorted(s))
+        # t = ''.join(sorted(t))
 
-        return s == t
+        return Counter(s) == Counter(t)
 
 # Test cases
 if __name__ == "__main__":
---
+++ p3.py
@@ -0,0 +1,25 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Your code here
+        if not strs:
+            return [[""]]
+        result = defaultdict(list)
+        for i in strs:
+            signature = ''.join(sorted(i))
+            result[signature].append(i)
+        return list(result.values())
+
+
+
+# Test cases
+if __name__ == "__main__":
+    solution = Solution()
+    
+    print(solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+    # Expected: [["bat"],["nat","tan"],["ate","eat","tea"]] (any order)
+    
+    print(solution.groupAnagrams([""]))
+    # Expected: [[""]]
